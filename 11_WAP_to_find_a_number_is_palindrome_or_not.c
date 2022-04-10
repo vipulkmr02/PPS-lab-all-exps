2d57c2797e8e320cc095dfaa07662f8d1f2a5e9b
Vipul Kumar <vipulkmr02@gmail.com>
2022-04-10 18:25:03 +0530

    syntax corrected in 11 exp

--- 11_WAP_to_find_a_number_is_palindrome_or_not.c
+++ 11_WAP_to_find_a_number_is_palindrome_or_not.c
@@ -1,11 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
     // declaring variables
     int i,
-    reverse = 0,
-    number,
-    original;
+        reverse = 0,
+        number,
+        original;
 
     // asking the user for the number
     printf("Enter a number\n");
@@ -13,19 +14,22 @@ int main() {
 
     // reversing the number
     original = number;
-    do {
+    do
+    {
         // last digit of a number is the remainder
         // of the number divided by 10
         int last_digit = number % 10;
         reverse = (reverse * 10) + last_digit;
-    }while(number > 0)
+    } while (number > 0);
 
     // if reversed is same as the original
     // then the number is palindrome
-    if (original == reverse) {
+    if (original == reverse)
+    {
         printf("It is a Palimdrome number.");
     }
-    else {
+    else
+    {
         printf("It is not a Palindrome number.");
     }
 
