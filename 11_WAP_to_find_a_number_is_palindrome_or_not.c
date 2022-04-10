275e69c2fa7e57fc1a3f0df4c79ebefd7baf99cd
Vipul Kumar <vipulkmr02@gmail.com>
2022-04-10 18:31:27 +0530

    11

--- 11_WAP_to_find_a_number_is_palindrome_or_not.c
+++ 11_WAP_to_find_a_number_is_palindrome_or_not.c
@@ -3,8 +3,7 @@
 int main()
 {
     // declaring variables
-    int i,
-        reverse = 0,
+    int reverse = 0,
         number,
         original;
 
@@ -19,6 +18,8 @@ int main()
         // last digit of a number is the remainder
         // of the number divided by 10
         int last_digit = number % 10;
+        // shortening the number for next time
+        number /= 10;
         reverse = (reverse * 10) + last_digit;
     } while (number > 0);
 
@@ -26,7 +27,7 @@ int main()
     // then the number is palindrome
     if (original == reverse)
     {
-        printf("It is a Palimdrome number.");
+        printf("It is a Palindrome number.");
     }
     else
     {
