97e25c2ad46d7e06630ba6b6a55ff6938048b4d5
Vipul Kumar <vipulkmr02@gmail.com>
2022-04-14 05:58:25 +0530

    uncompleted 17

---
+++ 17_WAP_to_sort_array_with_Insertion_sort.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    // To sort an array of size n in ascending order:
+    // 1: Iterate from arr[1] to arr[n] over the array.
+    // 2: Compare the current element (key) to its predecessor.
+    // 3: If the key element is smaller than its predecessor, 
+    // compare it to the elements before. Move the greater elements
+    // one position up to make space for the swapped element
+
+    // 5 1 4 2 8
+    
+
+    printf("running...\n");
+    return 0;
+}
